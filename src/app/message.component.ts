1b92bbd82053ec7c8d6b55b52ace3fe01ec297f5
Taipan Prasithpongchai <dewnoibkk@gmail.com>
2018-05-15 00:41:23 +0700

    Reformat code

--- src/app/message.component.ts
+++ src/app/message.component.ts
@@ -1,8 +1,8 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: "app-message",
-    template: "<h1>This is my message</h1>"
+    selector: 'app-message',
+    template: '<h1>This is my message</h1>'
 })
 export class MessageComponent {
 }
